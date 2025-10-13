099cc3d2d28c9d9e10724f03734df0ee772afba6
GwyfynSimple <sniffleslou@gmail.com>
2025-10-13 16:39:27 +0100

    built

--- src/main.cpp
+++ src/main.cpp
@@ -5,23 +5,40 @@ using namespace std;
 
 void cleanup(SDL_Window *win)
 {
-    SDL_DestoryWindow(win);
+    SDL_DestroyWindow(win);
     SDL_Quit();
 }
 
-int main (int argc, char * argv[]) {
-   if (SDL_Init(SDL_INIT_VIDEO) !=0{
+int main (int argc, char * argv[])
+{
+   if (SDL_Init(SDL_INIT_VIDEO) !=0)
+   {
         SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "error", "Error initializing sdl", nullptr);
         return 1;
     }
     int width = 800;
     int height = 600;
-    SDL_Window *win = SDL_CreateWindow("sdl demo", SDL_WINDOWPOS_CENTERED, SDL_WIDNDOWPOS_CENTERED, width, height, 0);
+    SDL_Window *win = SDL_CreateWindow("SDL Demo", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, width, height, 0);
 
     if (!win){
         SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "ERROR", "FAILED TO CREATE WINDOW", nullptr);
         SDL_Quit();
         return 1;
     }
+    //for testing, event loop
+    bool running = true;
+    SDL_Event event;
+    while(running)
+    {
+        while(SDL_PollEvent(&event))
+        {
+            if(event.type == SDL_QUIT){
+                running = false;
+            }
+        }
+
+    }
+    cleanup(win);
+    return 0;
 }
 
