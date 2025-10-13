93a54e5535217df4d2f085875196fa41104dd0a1
GwyfynSimple <sniffleslou@gmail.com>
2025-10-13 19:46:09 +0100

    Window pops up
    The code now opens the window up. My goal to is rename is to the project name, when add assests

--- src/main.cpp
+++ src/main.cpp
@@ -9,36 +9,39 @@ void cleanup(SDL_Window *win)
     SDL_Quit();
 }
 
-int main (int argc, char * argv[])
-{
-   if (SDL_Init(SDL_INIT_VIDEO) !=0)
-   {
-        SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "error", "Error initializing sdl", nullptr);
+int main (int argc, char * argv[]) {
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "error", "Error initialising sdl", nullptr);
         return 1;
     }
     int width = 800;
     int height = 600;
     SDL_Window *win = SDL_CreateWindow("SDL Demo", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, width, height, 0);
 
-    if (!win){
+    if (!win) {
         SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "ERROR", "FAILED TO CREATE WINDOW", nullptr);
         SDL_Quit();
         return 1;
     }
     //for testing, event loop
     bool running = true;
-    SDL_Event event;
-    while(running)
+    while (running)
     {
-        while(SDL_PollEvent(&event))
+        SDL_Event event;
+        while (SDL_PollEvent(&event))
         {
-            if(event.type == SDL_QUIT){
-                running = false;
+            switch (event.type)
+            {
+                case SDL_QUIT: {
+                    running = false;
+                    break;
+                    default:
+                        break;
+                }
             }
         }
-
     }
     cleanup(win);
     return 0;
-}
 
+}
