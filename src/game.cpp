4dfaaabd0df46f816d61dfdb780b4a9d567fad37
GwyfynSimple <sniffleslou@gmail.com>
2025-10-22 20:16:23 +0100

    Button placement and timer
    added a timer and move buttons making them bigger

--- src/game.cpp
+++ src/game.cpp
@@ -66,6 +66,8 @@ void Game::render() {
     SDL_RenderClear(renderer);
 
     //buttons
+    kickButtonReact = {100,400,256,128};
+    letInButtonReact = {450,400,256,128};
     SDL_RenderCopy(renderer, KickButtonTexture, nullptr, &kickButtonReact);
     SDL_RenderCopy(renderer, LetInButtonTexture, nullptr, &letInButtonReact);
 
--- src/rounds.cpp
+++ src/rounds.cpp
@@ -1,16 +1,22 @@
 #include <iostream>
 #include "Rounds.h"
 
-Rounds::Rounds() : CurrentRound(0),totalGuests(0),roundOver(0){}
+Rounds::Rounds() : CurrentRound(0),totalGuests(0),roundOver(0), roundStartTime(0), roundDuration(90000){}
 
 Rounds::~Rounds() {}
 
 void Rounds::StartRound() {
     roundOver = false;
     totalGuests = 3;
+    roundStartTime = SDL_GetTicks();
     std:: cout << "round " << CurrentRound << " started " << std::endl;
 }
 void Rounds::Update() {
+    Uint32 elapsedTime = SDL_GetTicks() - roundStartTime;
+    if(elapsedTime >= roundDuration){
+        roundOver = true;
+        std::cout << "round over!" << std::endl;
+    }
     totalGuests <=0;{
         roundOver = true;
         totalGuests +=2;
